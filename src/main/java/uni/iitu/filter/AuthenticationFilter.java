cbbae3f0e428be2d03dfce61333c7cc242795d86
Adil <adil.kenesbek21@gmail.com>
2021-03-15 03:18:13 +0600

    2nd filter

--- src/main/java/uni/iitu/filter/AuthenticationFilter.java
+++ src/main/java/uni/iitu/filter/AuthenticationFilter.java
@@ -1,11 +1,13 @@
 package uni.iitu.filter;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@WebFilter
 public class AuthenticationFilter implements Filter {
 
     private ServletContext context;
---
+++ src/main/java/uni/iitu/filter/RequestLoggingFilter.java
@@ -0,0 +1,42 @@
+package uni.iitu.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Enumeration;
+
+public class RequestLoggingFilter implements Filter {
+
+    private ServletContext context;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.context = filterConfig.getServletContext();
+        this.context.log("RequestLoggingFilter initialized");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        Enumeration<String> params = req.getParameterNames();
+        while(params.hasMoreElements()){
+            String name = params.nextElement();
+            String value = servletRequest.getParameter(name);
+            this.context.log(req.getRemoteAddr() + "::Request Params::{"+name+"="+value+"}");
+        }
+
+        Cookie[] cookies = req.getCookies();
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                this.context.log(req.getRemoteAddr() + "::Cookie::{"+cookie.getName()+","+cookie.getValue()+"}");
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
